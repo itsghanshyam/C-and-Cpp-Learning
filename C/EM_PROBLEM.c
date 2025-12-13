6979062ca6942ff61d907faa8bdbbc473ca431a1
Ghanshyam Matlane <68685017+itsghanshyam@users.noreply.github.com>
2025-12-13 03:44:50 +0000

    Engineering Mechanics C program for calculation minimum and maximum friction for a block on inclined plane.

---
+++ C/EM_PROBLEM.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    printf("This is a minimum and maximum force calculator\n"
+    "So the question is like this:\n\n"
+    "A block of mass m is resting on an inclined plane with inclination angle x\n"
+    "with the horizontal and coefficient of static friction between the surface\n"
+    "of plane and block is k\n\n");
+    return 0;
+}
