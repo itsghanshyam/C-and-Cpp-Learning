114bef488a91d0bcc6076ab9fce9f4f68e9f1a11
Ghanshyam <68685017+itsghanshyam@users.noreply.github.com>
2025-12-25 16:17:02 +0530

    Added logical.cpp

---
+++ C++/logical.cpp
@@ -0,0 +1,15 @@
+#include <iostream.h>
+using namespace std;
+int main(){
+  int num a,b;
+  cout << "Enter the value of a: "<< endl;
+  cin >> a;
+  cout << "Enter the value of b: "<< endl;
+  cin >> b;
+  if (a > b){
+    cout << "a is greater than b!" << endl;
+  else if cout << "a is less than b" << endl;
+  else cout << " a is equal to b"
+  }
+  return 0;
+}
